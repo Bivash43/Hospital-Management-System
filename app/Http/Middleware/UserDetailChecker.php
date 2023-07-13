b07b7d4ced2e01b4806455a2f18e6ac99758d06d
Bivash43 <bivash43e@gmail.com>
2023-07-13 10:08:34 +0545

    Dashboard Handling

---
+++ app/Http/Middleware/UserDetailChecker.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class UserDetailChecker
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!Auth::check()) {
+            return redirect()->route('login');
+        }
+
+        $user = Auth::user();
+
+        // if () {
+        //     return $next($request);
+        // }
+    }
+}
